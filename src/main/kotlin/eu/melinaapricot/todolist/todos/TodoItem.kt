7453ef47953330769f0d7684c009cf4de075079e
Alkis Mavridis <alkismavridis@gmail.com>
2021-10-23 19:40:08 +0200

    Introduce creation date

--- src/main/kotlin/eu/melinaapricot/todolist/todos/TodoItem.kt
+++ src/main/kotlin/eu/melinaapricot/todolist/todos/TodoItem.kt
@@ -1,9 +1,11 @@
 package eu.melinaapricot.todolist.todos
 
+import java.time.Instant
 import java.util.*
 
 data class TodoItem(
   val id: UUID = UUID.randomUUID(),
   val isDone: Boolean = false,
   val message: String,
+  val createdAt: Instant
 )
